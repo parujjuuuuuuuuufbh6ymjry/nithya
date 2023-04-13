91a9efcef1178468daa087779e5eeef74498bf79
parujjuuuuuuuuufbh6ymjry <129931735+parujjuuuuuuuuufbh6ymjry@users.noreply.github.com>
2023-04-12 22:11:44 -0700

    Update java.java

--- java.java
+++ java.java
@@ -1,7 +1,9 @@
 public class java{
 	public static void main(String args[])
 	{
-		System.out.println("hello World")
+		System.out.println("hello World") 
+		System.out.println("hello java")	
+			
 	}
 }
 
